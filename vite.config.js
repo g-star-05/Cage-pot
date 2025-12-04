b94ce72a256710e5777dea0852d4fd9e55aed2e8
Gaurav Tare <gauravtare2002@gmail.com>
2025-12-04 16:59:36 +0530

    changes done

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// ⚠️ put YOUR repo name here
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/cage-pot/", // <-- if your repo is "cage-pot"
+});
